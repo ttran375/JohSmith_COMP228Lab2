bbce735353fa3d870cc8e5f1cfbeac2b824ab7d9
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-02 14:16:42 +0000

    Update solutions

--- src/exercise1/Main.java
+++ src/exercise1/Main.java
@@ -1,23 +1,4 @@
 /**
- * This is the main class for the exercise1 package.
+ * Contains classes for the first exercise.
  */
-package exercise1;
-
-public class Main {
-    /**
-     * Private constructor to prevent instantiation.
-     */
-    private Main() {
-        // prevents instantiation
-    }
-
-    /**
-     * This is the main method which makes use of inputAnswer method.
-     * 
-     * @param args Unused.
-     */
-    public static void main(final String[] args) {
-        Test test = new Test();
-        test.inputAnswer();
-    }
-}
+package exercise2;
