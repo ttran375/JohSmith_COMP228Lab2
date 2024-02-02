3a200e9641b1654d36e59ef679679ceb855f6e86
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-02 14:39:56 +0000

    Fri, Feb 2, 2024, 2:39 PM +00:00

--- src/exercise1/Test.java
+++ src/exercise1/Test.java
@@ -1,46 +1,50 @@
 package exercise1;
 
-import javax.swing.JOptionPane;
 import java.util.Random;
+import javax.swing.JOptionPane;
 
 /**
  * This class represents a test with multiple choice questions.
  */
 public final class Test {
+
     private static final int MAX_ANSWER = 4;
 
     /**
      * Array of questions for the test.
      */
     private final String[] questions = {
-            "Question 1: Which of the following tools support the...",
-            "Question 2: What is the main function of a Java...",
-            "Question 3: Which of the following components...",
-            "Question 4: Which of the following typically...",
-            "Question 5: There are variables for which..."
+        "Question 1: Which of the following tools support the...",
+        "Question 2: What is the main function of a Java...",
+        "Question 3: Which of the following components...",
+        "Question 4: Which of the following typically...",
+        "Question 5: There are variables for which...",
     };
 
     /**
      * Array of options for each question in the test.
      */
     private final String[][] options = {
-            { "1. JDK", "2. JVM", "3. IDE", "4. API" },
-            { "1. Translates source code",
-                    "2. Places program in memory",
-                    "3. Executes bytecodes",
-                    "4. Examines bytecodes" },
-            { "1. Local variable",
-                    "2. Instance variable",
-                    "3. Non-static Method",
-                    "4. Class" },
-            { "1. Package",
-                    "2. Method",
-                    "3. IDE",
-                    "4. Function" },
-            { "1. class variables",
-                    "2. constants",
-                    "3. local variables",
-                    "4. instance variables" }
+        { "1. JDK", "2. JVM", "3. IDE", "4. API" },
+        {
+            "1. Translates source code",
+            "2. Places program in memory",
+            "3. Executes bytecodes",
+            "4. Examines bytecodes",
+        },
+        {
+            "1. Local variable",
+            "2. Instance variable",
+            "3. Non-static Method",
+            "4. Class",
+        },
+        { "1. Package", "2. Method", "3. IDE", "4. Function" },
+        {
+            "1. class variables",
+            "2. constants",
+            "3. local variables",
+            "4. instance variables",
+        },
     };
 
     /**
@@ -63,10 +67,16 @@ public final class Test {
         String[] option = options[questionNumber];
         String answer;
         do {
-            answer = JOptionPane.showInputDialog(null, question + "\n" + String.join("\n", option));
+            answer =
+                JOptionPane.showInputDialog(
+                    null,
+                    question + "\n" + String.join("\n", option)
+                );
             if (!isValidAnswer(answer)) {
-                JOptionPane.showMessageDialog(null,
-                        "Invalid input. Please enter a number between 1 and 4.");
+                JOptionPane.showMessageDialog(
+                    null,
+                    "Invalid input. Please enter a number between 1 and 4."
+                );
             }
         } while (!isValidAnswer(answer));
         checkAnswer(questionNumber, answer);
@@ -118,7 +128,8 @@ public final class Test {
                 message = isCorrect ? "Good!" : "Wrong. Try once more";
                 break;
             case 2:
-                message = isCorrect ? "Keep up the good work!" : "Don't give up!";
+                message =
+                    isCorrect ? "Keep up the good work!" : "Don't give up!";
                 break;
             default:
                 message = isCorrect ? "Nice work!" : "No. Keep trying..";
@@ -134,6 +145,9 @@ public final class Test {
         for (int i = 0; i < questions.length; i++) {
             simulateQuestion(i);
         }
-        JOptionPane.showMessageDialog(null, "You scored " + score + " out of " + questions.length);
+        JOptionPane.showMessageDialog(
+            null,
+            "You scored " + score + " out of " + questions.length
+        );
     }
 }
