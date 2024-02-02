57bd8787e777d51852a43ad88b8da46bcfa82d83
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-02 15:56:50 +0000

    Fri, Feb 2, 2024, 3:56 PM +00:00

--- src/exercise1/Main.java
+++ src/exercise1/Main.java
@@ -1,4 +1,25 @@
+// File: exercise1/package-info.java
 /**
- * Contains classes for the first exercise.
+ * This package contains the Main class for exercise 1.
  */
-package exercise2;
+package exercise1;
+
+/**
+ * This is the main class for exercise 1.
+ */
+public final class Main {
+
+    private Main() {
+        // Prevent instantiation
+    }
+
+    /**
+     * The entry point of the application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(final String[] args) {
+        Test test = new Test();
+        test.inputAnswer();
+    }
+}
