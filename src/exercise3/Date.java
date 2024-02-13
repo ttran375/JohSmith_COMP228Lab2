c379456d8cd065bb6b9ebc2bc44fee1d18ea78e1
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-13 10:48:54 +0000

    Tue, Feb 13, 2024, 10:48 AM +00:00

--- src/exercise3/Date.java
+++ src/exercise3/Date.java
@@ -81,8 +81,34 @@ public class Date {
      * @return The three-letter abbreviation of the month.
      */
     public static String getMonthText(int month) {
-        // ... (unchanged)
-
+        switch (month) {
+            case 1:
+                return "Jan";
+            case 2:
+                return "Feb";
+            case 3:
+                return "Mar";
+            case 4:
+                return "Apr";
+            case 5:
+                return "May";
+            case 6:
+                return "Jun";
+            case 7:
+                return "Jul";
+            case 8:
+                return "Aug";
+            case 9:
+                return "Sep";
+            case 10:
+                return "Oct";
+            case 11:
+                return "Nov";
+            case 12:
+                return "Dec";
+            default:
+                return "Unknown";
+        }
     }
 
     /**
@@ -93,7 +119,17 @@ public class Date {
      * @return The number of days in the specified month.
      */
     private static int getDaysInMonth(int year, int month) {
-        // ... (unchanged)
+        switch (month) {
+            case 2:
+                return isLeapYear(year) ? 29 : 28;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            default:
+                return 31;
+        }
     }
 
     /**
@@ -103,7 +139,7 @@ public class Date {
      * @return True if the year is a leap year, false otherwise.
      */
     private static boolean isLeapYear(int year) {
-        // ... (unchanged)
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 
     private void normalize() {
